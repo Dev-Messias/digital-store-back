77c65a9396d4ebcae24e6ec3119f3db653e86157
DevMessias <messiasbento944@gmail.com>
2024-08-13 17:37:18 -0300

    🚀~Feat: Tratando erros

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express, {Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 
 //importando as rotas
 import { router } from './routes';
@@ -8,7 +10,26 @@ const app = express();
 //vamos o utilizar o tipo de dado como json
 app.use(express.json());
 
+//permitindo acessar de qualquer ip/url
+app.use(cors())
+
 //utilizar todas as rotas do arquivo rotas
 app.use(router);
+
+//tratando error
+app.use((err: Error, req: Request, res:Response, next: NextFunction)=>{
+    //verificando se o que esta passando na rota é do tipo erro
+    if(err instanceof Error){
+        //se for uma instacia do tipo error
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+})
 
 app.listen(3333, () => console.log('Servidor online!'))
